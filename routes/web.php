3ae7c6ca3e880efae899c9d1218c0afef313a5a3
kinshuk <webgrity158@gmail.com>
2025-10-15 18:53:56 +0530

    Revert "update"
    This reverts commit 905443aaec031d0d8488fda749d26b3f123b807e.

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,9 @@
 <?php
 
-use App\Events\TestEvent;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\TestController;
 use App\Http\Middleware\AuthMiddleware;
-use Illuminate\Broadcasting\Channel;
-use Illuminate\Support\Facades\Broadcast;
-use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -34,16 +30,5 @@ Route::middleware([AuthMiddleware::class])->controller(HomeController::class)->p
 Route::controller(TestController::class)->prefix('test')->group(function() {
     Route::get('/zip2pdf', [TestController::class, 'index'])->name('zip2pdf.index');
     Route::post('/zip2pdf/process', [TestController::class, 'process'])->name('zip2pdf.process');
-    Route::get('socket', function () {
-        return view('test.socket');
-    });
-
-    Route::get('socket-hit', function () {
-        event(new TestEvent([
-            'test' => "Jjhfjkhdfkj",
-            'gshg' => 1234567890
-        ]));
-        return 'Hit..';
-    });
 
 });
